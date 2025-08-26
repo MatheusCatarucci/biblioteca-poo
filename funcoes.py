52a82ad6219204034047f23e659f044732605b95
MoisesTafarello <moisesgabrieltafarello5@gmail.com>
2025-08-26 09:48:07 -0300

    Colocando os Setters

--- funcoes.py
+++ funcoes.py
@@ -1,9 +1,19 @@
 class Livros:
-    def __init__(self, livro, autor, genero, disponivel = True):
-        self.livro = livro
+    def __init__(self, titulo, autor, genero, disponivel = True):
+        self.titulo = titulo
         self.autor = autor
         self.genero = genero
         self.disponivel = disponivel
 
 # ----------------------------------------------------------------------------------------------------------------------------
+    def set_titulo(self, novo_titulo):
+        self._titulo = novo_titulo
+    
+    def set_autor (self, novo_autor):
+        self.autor = novo_autor
 
+    def set_genero(self, novo_genero):
+        self.set_genero = novo_genero
+
+    def set_disponivel (self, status):
+        self.disponivel = status
