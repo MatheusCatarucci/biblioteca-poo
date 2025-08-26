4a41632f3f112a88fa86c5c72009ceb7dae62b4c
MoisesTafarello <moisesgabrieltafarello5@gmail.com>
2025-08-26 08:33:49 -0300

    Iniciando Classe dos Livros

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+class Livros:
+    def __init__(self, livro, autor, genero, disponivel = True):
+        self.livro = livro
+        self.autor = autor
+        self.genero = genero
+        self.disponivel = disponivel
+
+# ----------------------------------------------------------------------------------------------------------------------------
+
