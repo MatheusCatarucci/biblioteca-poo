5a5c11de0e0369268eea32de9b9e48b24b3bf553
MoisesTafarello <moisesgabrieltafarello5@gmail.com>
2025-08-26 10:07:03 -0300

    update

--- funcoes.py
+++ funcoes.py
@@ -1,19 +1,19 @@
 class Livros:
     def __init__(self, titulo, autor, genero, disponivel = True):
-        self.titulo = titulo
-        self.autor = autor
-        self.genero = genero
-        self.disponivel = disponivel
+        self._titulo = titulo
+        self._autor = autor
+        self._genero = genero
+        self._disponivel = disponivel
 
 # ----------------------------------------------------------------------------------------------------------------------------
     def set_titulo(self, novo_titulo):
         self._titulo = novo_titulo
     
     def set_autor (self, novo_autor):
-        self.autor = novo_autor
+        self._autor = novo_autor
 
     def set_genero(self, novo_genero):
-        self.set_genero = novo_genero
+        self._genero = novo_genero
 
     def set_disponivel (self, status):
-        self.disponivel = status
+        self._disponivel = status
