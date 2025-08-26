b38c192709ae302142d460f5422266903b8511bb
Guilherme Ballestrim Sobreira <gui.ballestrim@gmail.com>
2025-08-26 08:54:53 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-class Livro:
-    #Construtor
-    def __init__(self, titulo, autor, genero, status):
-        self.__titulo = titulo
-        self.__autor = autor
-        self.__genero = genero
-        self.__status = status
-    # -----------------------------------------------------------------------
-    # Métodos
-    def getTitulo(self):
-        return self.__titulo
-    def getAutor(self):
-        return self.__autor
-    def getGenero(self):
-        return self.__genero
-    def getStatus(self):
-        return self.__status
-    
-livros = {Livro('Senhor dos Anéis', 'J. R. R. Tolkien', 'Ficção', 0)}
-print(livros.getAutor())
-=======
 input(" ____________________________\n |                          |\n |                          |\n |   Empréstimo dos Livros  |\n |                          |\n |__________________________|")
 print("")
 emprestimo=input("Você deseja emprestar um livro? Digite Sim ou Não: ").strip().lower()
